951652e2b164bccb9e7df3ca0828fc15dadcc4dd
Your Name <you@example.com>
2022-08-17 16:56:23 -0300

    importando navbar e sidebar para minha home e estilizando alguns detalhes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import './home.scss'
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='home'>
+      <Sidebar />
+      <hr />
+    <div className="homeContainer">
+      <Navbar />
     </div>
+    </div>
+
   )
 }
 
