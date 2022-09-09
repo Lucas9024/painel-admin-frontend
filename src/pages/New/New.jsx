4acebaa05c1c70e59c05412d57126347a9f4e811
Your Name <you@example.com>
2022-09-09 16:01:38 -0300

    Criando os dados de usuários e produtos

--- src/pages/New/New.jsx
+++ src/pages/New/New.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import Navbar from '../../components/Navbar/Navbar'
+import './new.scss'
 
-const New = () => {
+const New = ({inputs, title}) => {
   return (
-    <div>
-      New
+    <div className='container'>
+      <Sidebar />
+
+          <div className='content'>
+
+        <Navbar />
+
+        <div className="blockTop">
+          <span className='textUser'>
+          {title}
+          </span>
+        </div>
+        <div className='blockBottom'>
+
+          </div>
+
+        </div>
     </div>
   )
 }
